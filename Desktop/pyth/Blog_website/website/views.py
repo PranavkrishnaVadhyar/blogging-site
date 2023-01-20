0a1cfcf6eb72925909ea3ad54a8b6653eb2d0dd4
PranavkrishnaVadhyar <bpranavkrishnavadhyar@gmail.com>
2023-01-20 21:40:56 +0530

    posts1

---
+++ Desktop/pyth/Blog_website/website/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint,render_template,request,flash
+from flask_login import login_required,current_user
+
+views = Blueprint("views",__name__)
+
+@views.route('/')
+@views.route("/home")
+@login_required
+def home():
+    return render_template("home.html", user = current_user)
+
+
+@views.route("/create-post", methods=['GET','POST'])
+@login_required
+def create_post():
+    
+    if request.method == "POST":
+        text = request.form.get('text')
+        
+        if not text:
+            flash('Post Created', category = "error")
+        
+        else:
+            flash('Post created!',category = 'success')
+            
+        
+    return render_template('create_posts.html',user=current_user)
